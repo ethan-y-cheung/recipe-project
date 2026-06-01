c3a3b3d7340dee4359680ff3bd6f18764ca07974
angelinaa-rodriguez <134556378+angelinaa-rodriguez@users.noreply.github.com>
2026-06-01 13:00:14 -0500

    update navbar with logo, auth buttons, and routing links

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,17 +1,22 @@
 import { Link } from 'react-router-dom'
-import './Navbar.css'
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <span className="navbar-logo">Recipe App</span>
+      <div className="navbar-brand">
+        <div className="navbar-logo-circle">🌿</div>
+        <span className="navbar-title">Recipe</span>
+      </div>
       <ul className="navbar-links">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/recipes">Recipes</Link></li>
         <li><Link to="/create">Create Recipe</Link></li>
         <li><Link to="/my-recipes">My Recipes</Link></li>
-        <li><Link to="/admin">Admin</Link></li>
       </ul>
+      <div className="navbar-auth">
+        <button className="nav-btn-login">Log In</button>
+        <button className="nav-btn-signup">Sign Up</button>
+      </div>
     </nav>
   )
 }
