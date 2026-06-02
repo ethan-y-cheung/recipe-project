f7006f4a88fb25f4384460f059939ea6ecbef049
VincentChan22 <VincentHChan22@gmail.com>
2026-06-02 11:01:05 -0400

    added test router to app.js

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,15 @@ import cors from "cors";
 import "dotenv/config";
 import express from "express";
 
+import testRouter from "./routes/test.js";
+
 const app = express();
 const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use('/', testRouter);
+app.use('/recipes', testRouter);
 
 app.get("/health", (req, res) => {
     res.json({ status: "ok" });
