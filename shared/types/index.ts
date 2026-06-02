dbe0b8c50aceb2676a697ddeae2270c7cdb0c3c6
VincentChan22 <VincentHChan22@gmail.com>
2026-06-02 09:56:08 -0400

    Added Canva Database and Type planning to shared types

---
+++ shared/types/index.ts
@@ -0,0 +1,32 @@
+export interface UserRecipeNotes
+{
+    recipeID: string;
+    user_tags: string[];
+    notes:string;
+}
+export interface User{
+    username: string;
+    password: string;
+    admin: boolean;
+    my_recipes: UserRecipeNotes[];
+    saved_recipes: UserRecipeNotes[];
+}
+export interface Recipe{
+    recipe_ID: string;
+    user_generated: boolean;
+    title : string;
+    tags_ids: string[];
+    ingredients: {
+        name: string;
+        quantity: string;
+    }[];
+    images: string[]; //first index is always thumbnail
+    rating: (1 | 2 | 3 | 4 | 5)[];
+}
+export interface Tag{
+    name: string;
+    type: string;
+}
+export interface Comments{
+    recipe_ID: string;
+}
