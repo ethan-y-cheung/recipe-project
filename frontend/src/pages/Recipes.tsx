4aa2ab5eee46389f56e47be2646ec97d8a68af08
Yuwen Zhang <yuwen.x.zhang@gmail.com>
2026-06-02 14:44:38 -0700

    chore: rebased with main

--- frontend/src/pages/Recipes.tsx
+++ frontend/src/pages/Recipes.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useState } from 'react'
 import '../styles/Recipes.css'
 
@@ -169,6 +170,25 @@ export default function Recipes() {
     </section>
 
     </main>
+=======
+import './Recipes.css'
+
+export default function Recipes() {
+  return (
+    <header className="recipes-heading">
+      <div className="recipes-heading__text">
+        <h2 className="recipes-heading__title">Explore <span className="">Recipes</span></h2>
+        <p className="recipes-heading__subtitle">
+          Find your next favorite meal from our growing recipe collection.
+        </p>
+      </div>
+      <img
+        className="recipes-heading__image"
+        src="https://media-cldnry.s-nbcnews.com/image/upload/t_social_share_1200x630_center,f_auto,q_auto:best/newscms/2024_28/2067113/keep-it-zesty-chicken-shawarma-2x1-mc-240712.jpg"
+        alt="An assortment of dishes spread across a table"
+      />
+    </header>
+>>>>>>> 8d845cf (chore: rebased with main)
   )
 }
 
