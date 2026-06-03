7f9b34cfd2208dae326c8b4be99d77b367dade32
angelinaa-rodriguez <134556378+angelinaa-rodriguez@users.noreply.github.com>
2026-06-03 14:10:06 -0500

    turn off eslint rule

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -28,4 +28,10 @@ export default defineConfig([
       'react-refresh/only-export-components': 'off',
     },
   },
+  {
+    files: ['src/contexts/**/*.{ts,tsx}', 'src/components/ui/**/*.{ts,tsx}'],
+    rules: {
+      'react-refresh/only-export-components': 'off',
+    },
+  },
 ])
