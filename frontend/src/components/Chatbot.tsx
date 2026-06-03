a49c6cda57e268d4da5f1667a52a825bd45cdbb5
Emily Middleton <emiddleton@smith.edu>
2026-06-03 14:18:49 -0500

    feat: chatbot implementation

--- frontend/src/components/Chatbot.tsx
+++ frontend/src/components/Chatbot.tsx
@@ -1,11 +1,47 @@
 import "../styles/RecipeDetail.css";
-import {Search, X} from "lucide-react";
+import {X} from "lucide-react";
+import {useState} from 'react';
+import axios from 'axios';
 
 type ChatbotProps = {
   setClose: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
+interface ChatbotMessage {
+  role: string;
+  content: string;
+}
+
 const Chatbot = ( { setClose } : ChatbotProps ) => {
+  const baseURL : string = import.meta.env.VITE_BASE_URL || 'http://localhost:5001';
+  const [messages, setMessages] = useState<ChatbotMessage[]>([{role: "bot", content: "ask any cooking questions here!"}]);
+  const [search, setSearch] = useState<string>("");
+
+  async function sendMessage() {
+    if (!search.trim()) return; // no empty searches
+
+    const newMessages = [
+      ...messages,
+      { role: "user", content: search},
+    ];
+    setMessages(newMessages);
+
+    try {
+      const response = await axios.post(`${baseURL}/chat`,
+        {messages: newMessages,}
+      )
+
+      setMessages([
+        ...newMessages,
+        response.data,
+      ]);
+      
+      setSearch(""); // clear out search
+    } catch (error) {
+      console.error("Error in server:", error);
+    }
+  }
+
   return (
     <>
       <section className="chat-popup">
@@ -13,8 +49,19 @@ const Chatbot = ( { setClose } : ChatbotProps ) => {
           <p className="chat-header-title">Recipe Help</p>
         </header>
 
-        <input type="text" className="chat-searchbar"></input>
-        <Search className="pin-to-searchbar"/>
+        {messages.map((message, index) => (
+          <div className={`message ${message.role === "user" ? "user" : "bot"}`} key={index}>
+            <p>
+              {message.content}
+            </p>
+          </div>))}
+
+
+        <input placeholder="substitutes for onions..." 
+        type="text" 
+        onChange={(e) => setSearch(e.target.value)}
+        onKeyDown={(e) => {if (e.key === "Enter") sendMessage()}}
+        className="chat-searchbar"></input>
         <X className="exit-button" onClick={() => setClose(prevState=>!prevState)}/>
       </section>
     </>
