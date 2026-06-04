68602a62dad89a71802ab405c3fd17c2e2c3a130
Emily Middleton <emiddleton@smith.edu>
2026-06-04 10:48:31 -0500

    fix: typescript compilation error fix

---
+++ backend/types/index.ts
@@ -0,0 +1,51 @@
+export interface UserRecipeNotes
+{
+    recipeID: string;
+    user_tags: string[];
+    notes:string;
+}
+export interface User{
+    username: string;
+    password: string;
+    admin: boolean;
+    my_recipes: UserRecipeNotes[];
+    saved_recipes: UserRecipeNotes[];
+}
+export interface Recipe{
+    id: string;
+    user_generated: boolean;
+    creator_ID?: string | null;
+    title : string;
+    created_at?: Date | null;
+    approved: boolean;
+    tags: Tag[];
+    ingredients: {
+        name: string;
+        quantity: string;
+    }[];
+    instructions: string[];
+    images: string[]; //first index is always thumbnail
+    imageUrls?: (string | null)[];
+    rating: Rating[];
+    total_time?: string | null;
+    servings?: number | null; // potentially null because api doesn't include these fields
+
+}
+export interface Rating{
+    user_ID: string;
+    value : (null | 1 | 2 | 3 | 4 | 5);
+}
+export interface Tag{
+    name: string;   
+    type: string;
+}
+export interface Comments{
+    id: string;
+    recipe_ID: string;
+    creator_ID: string
+    content: string;
+    likes: string[]; // array of user_IDs who liked the comment
+    created_at: Date;
+    replies: Comments[]; //array of replies to the comment, which are also of type Comments
+    reply_IDs: string[];
+}
