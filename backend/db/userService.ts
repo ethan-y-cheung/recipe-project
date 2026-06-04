3555ae0d1557b4718b4f75abc597e0739971e364
Sajid Islam <sajnyc360@gmail.com>
2026-06-04 14:34:33 -0400

    feat: setup routes for getting saved and created recipes

--- backend/db/userService.ts
+++ backend/db/userService.ts
@@ -1,34 +1,40 @@
-// import {
-//     collection,
-//     doc,
-//     getDoc,
-//     getDocs,
-//     updateDoc,
-//     deleteDoc,
-//     query,
-//     where,
-// } from "firebase/firestore";
+import {
+    collection,
+    doc,
+    getDoc,
+    getDocs,
+    updateDoc,
+    deleteDoc,
+    query,
+    where,
+} from "firebase/firestore";
+import db from "../firebase.js";
+import { Recipe } from "../../shared/types/index.ts";
 
-// import db from "../firebase.js";
-// import { User, Recipe } from "../../shared/types/index.ts";
+export const getCreatedRecipes = async (
+    username: string,
+): Promise<Recipe[]> => {
+    const q = query(
+        collection(db, "recipes"),
+        where("creator_ID", "==", username),
+    );
+    const snapshot = await getDocs(q);
 
-// export const getUserById = async (username: string) => {
-//     const docRef = doc(db, "users", username);
+    return snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+    })) as Recipe[];
+};
 
-//     const snapshot = await getDoc(docRef);
-//     if (!snapshot.exists()) return null;
+export const getSavedRecipes = async (
+    username: string,
+): Promise<string[] | null> => {
+    const snapshot = await getDoc(doc(db, "users", username));
 
-//     return snapshot.data() as User;
-// };
+    if (!snapshot.exists()) return null;
 
-// export const getCreatedRecipes = async (username: string) => {
-//     const snapshot = await getDocs(
-//         collection(db, "recipes"),
-//         where("creator_ID", "==", username),
-//     );
+    const userData = snapshot.data();
+    const savedRecipes = userData?.saved_recipes || [];
 
-//     return snapshot.docs.map((doc) => ({
-//         recipe_ID: doc.id,
-//         ...doc.data(),
-//     })) as Recipe[];
-// };
+    return savedRecipes.map((item: any) => item.recipe_id);
+};
---
+++ backend/routes/userRecipes.ts
@@ -0,0 +1,53 @@
+import { Router } from "express";
+import { getSavedRecipes, getCreatedRecipes } from "../db/userService.ts";
+import { fetchMultipleRecipes } from "../services/recipeService.ts";
+
+const router = Router();
+
+router.get("/:username/created", async (req, res) => {
+    try {
+        const { username } = req.params;
+        if (!username) {
+            return res.status(400).json({ error: "missing username" });
+        }
+
+        const createdRecipes = await getCreatedRecipes(username);
+
+        return res.status(200).json(createdRecipes);
+    } catch (error: any) {
+        console.error("Failed to fetch created recipes: ", error);
+        return res
+            .status(500)
+            .json({ message: "Failed to fetch created recipes: ", error });
+    }
+});
+
+router.get("/:username/saved", async (req, res) => {
+    try {
+        const { username } = req.params;
+        if (!username) {
+            return res.status(400).json({ error: "missing username" });
+        }
+
+        const savedIds = await getSavedRecipes(username);
+
+        if (savedIds === null) {
+            return res.status(404).json({ message: "user not found" });
+        }
+        if (savedIds.length === 0) {
+            return res.status(200).json([]);
+        }
+
+        const savedRecipes = await fetchMultipleRecipes(savedIds);
+
+        return res.status(200).json(savedRecipes);
+    } catch (error: any) {
+        console.error("Failed to fetch saved recipes: ", error);
+        return res.status(500).json({
+            messsage: "Failed to fetch saved recipes: ",
+            error,
+        });
+    }
+});
+
+export default router;
